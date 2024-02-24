426c0e39cf8a903d2de541f5d590a9a358bb3004
kx100 <off.blazee@gmail.com>
2024-02-24 17:06:21 -0500

    added js file

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    title: "Maryland",
+    link: "https://unsplash.com/photos/man-in-black-jacket-riding-on-motorcycle-on-road-during-daytime-EIN81690PWM",
+  },
+  {
+    title: "Philidelphia",
+    link: "https://unsplash.com/photos/city-skyline-during-night-time-PORWKRAI55U",
+  },
+  {
+    title: "Los Angeles",
+    link: "https://unsplash.com/photos/red-vehicle-on-the-road-photograph-Zvnf63bvOxk",
+  },
+  {
+    title: "Washington D.C.",
+    link: "https://unsplash.com/photos/a-pole-with-a-street-sign-on-it-in-front-of-a-building-FPWNqgCw2",
+  },
+  {
+    title: "Florida",
+    link: "https://unsplash.com/photos/brown-brick-pathway-eYA4DHQi8BI",
+  },
+  {
+    title: "New York",
+    link: "https://unsplash.com/photos/cars-on-road-K9set5TFE5U",
+  },
+];
+
+console.log(initialCards);
