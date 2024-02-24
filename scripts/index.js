b1d34a3817a57c765c4764fa3d1d807762bbd176
kx100 <off.blazee@gmail.com>
2024-02-24 18:37:31 -0500

    js fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const initialCards = [
   {
-    title: "Maryland",
+    name: "Maryland",
     link: "https://unsplash.com/photos/man-in-black-jacket-riding-on-motorcycle-on-road-during-daytime-EIN81690PWM",
   },
   {
-    title: "Philidelphia",
+    name: "Philidelphia",
     link: "https://unsplash.com/photos/city-skyline-during-night-time-PORWKRAI55U",
   },
   {
-    title: "Los Angeles",
+    name: "Los Angeles",
     link: "https://unsplash.com/photos/red-vehicle-on-the-road-photograph-Zvnf63bvOxk",
   },
   {
-    title: "Washington D.C.",
+    name: "Washington D.C.",
     link: "https://unsplash.com/photos/a-pole-with-a-street-sign-on-it-in-front-of-a-building-FPWNqgCw2",
   },
   {
-    title: "Florida",
+    name: "Florida",
     link: "https://unsplash.com/photos/brown-brick-pathway-eYA4DHQi8BI",
   },
   {
-    title: "New York",
+    name: "New York",
     link: "https://unsplash.com/photos/cars-on-road-K9set5TFE5U",
   },
 ];
