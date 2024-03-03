2c2927f53839c55e88c060a4b8d77010fe4c095a
kx100 <off.blazee@gmail.com>
2024-03-02 19:50:12 -0500

    js update

--- scripts/index.js
+++ scripts/index.js
@@ -27,13 +27,31 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#modal-edit-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
 
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal__opened");
 });
 
-const profileModalCloseButton = profileEditModal("#modal-closed-button");
+profileEditCloseButton.addEventListener("click", () => {
+  closePopup();
+});
 
-profileModalCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__opened");
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
 });
